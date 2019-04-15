0048ff69ad26599e92581fb7288d380c4d5f456e
godsenal <tmqps78@gmail.com>
2019-04-16 00:22:15 +0900

    Update variable order

--- js/constants.js
+++ js/constants.js
@@ -5,10 +5,11 @@ const queryForPath = {
 };
 const discoverWrapper = "[class*=discover-repositories]";
 
+const currentPath = window.location.pathname;
+const runtime = chrome.runtime;
+
 const defaultTitle = "h1, h2, h3";
 const defaultDescription =
   currentPath === "/discover" ? "[itemprop=description]" : ".text-gray";
-const currentPath = window.location.pathname;
-const runtime = chrome.runtime;
 
 const removed = new Set(); // save removed chinese repo for recover
